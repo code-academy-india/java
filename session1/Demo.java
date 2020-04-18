ccf0452a827f1aa81261654c41926e5e9b4feb19
Vasantha Kumaraswamy <vasntha.kumar@codeacademy.co.in>
2020-04-18 10:31:02 +0530

    session1 commit

---
+++ session1/Demo.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @author Vasantha
+ * Print multiple lines in Console Output
+ * 
+ */
+public class Demo {
+
+	public static void main(String[] args) {
+		//print on new line
+		System.out.println("ABC Technologies India Pvt Ltd");
+		System.out.println("------------------------------");
+		System.out.println("No: 1, MG Road");
+		
+		//print in same line
+		System.out.print("Bangalore");
+		System.out.print("India");
+		
+		
+
+	}
+
+}
---
+++ session1/Hello.java
@@ -0,0 +1,13 @@
+
+/**
+ * 
+ * @author Vasantha
+ * Java code to print Hello World
+ */
+public class Hello { 
+     public static void main(String[ ] args) {                  
+             System.out.println("Hello World"); 
+     }
+}
+
+
